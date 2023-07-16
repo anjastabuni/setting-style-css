cfdcfd11c0e72f88e6df1bbec18ea4cf65e721bd
username_anjastabuni <reblytabuni2004@gmail.com>
2023-07-16 19:08:23 +0700

    pengaturan style css dalam vue js

---
+++ app.js
@@ -0,0 +1,45 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    maximum: 50,
+    products: null,
+    cart: [],
+    style: {
+      label: ["font-weight-bold", "mr-2"],
+      inputWidth: 60,
+      sliderStatus: true,
+    },
+  },
+  mounted: function () {
+    fetch("https://hplussport.com/api/products/order/price")
+      .then((Response) => Response.json())
+      .then((data) => {
+        this.products = data;
+      });
+  },
+  computed: {
+    sliderState: function () {
+      return this.style.sliderStatus ? "d-flex" : "d-none";
+    },
+  },
+  methods: {
+    before: function (el) {
+      el.className = "d-none";
+    },
+    enter: function (el) {
+      var delay = el.dataset.index * 100;
+      setTimeout(function () {
+        el.className = "row d-flex mb-3 align-items-center animated fadeInRight";
+      }, delay);
+    },
+    leave: function (el) {
+      var delay = el.dataset.index * 100;
+      setTimeout(function () {
+        el.className = "row d-flex mb-3 align-items-center animated fadeOutRight";
+      }, delay);
+    },
+    addItem: function (product) {
+      this.cart.push(product);
+    },
+  },
+});
